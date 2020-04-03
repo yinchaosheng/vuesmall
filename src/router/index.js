88d5eaea946d4ac2ed513f6718aa6817b0040f1e
yinchaosheng <245394598@qq.com>
2020-04-03 16:20:54 +0800

    改成使用懒加载模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
-Vue.use(VueRouter)
+//import Home from '../views/Home.vue'
+//使用懒加载模式
+const Home = () => import('@/views/Home.vue');
+const About = () => import('@/views/About.vue');
+//注册vue路由
+Vue.use(VueRouter);
 
 const routes = [
   {
@@ -16,14 +19,15 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+      component: About
   }
-]
+];
 
 const router = new VueRouter({
+    //设置为history模式
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
 export default router
